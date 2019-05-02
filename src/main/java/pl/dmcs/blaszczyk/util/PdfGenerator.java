f87bc1e4cd5f6e76d09e1bba4fcde0a82b66e607
lukasz <lkszblaszczyk@gmail.com>
2019-05-02 23:29:33 +0200

    pdf generator

--- src/main/java/pl/dmcs/blaszczyk/util/PdfGenerator.java
+++ src/main/java/pl/dmcs/blaszczyk/util/PdfGenerator.java
@@ -18,46 +18,16 @@ public class PdfGenerator {
         Document document = new Document();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
-            PdfPTable table = new PdfPTable(3);
-            table.setWidthPercentage(60);
-            table.setWidths(new int[]{1, 3, 3});
-            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-
-            PdfPCell hcell;
-            hcell = new PdfPCell(new Phrase("Id", headFont));
-            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(hcell);
-
-            hcell = new PdfPCell(new Phrase("Name", headFont));
-            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(hcell);
-
-            hcell = new PdfPCell(new Phrase("Population", headFont));
-            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(hcell);
-
-            PdfPCell cell;
-            cell = new PdfPCell(new Phrase(String.valueOf((bill.getId()))));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(String.valueOf(bill.getElectricityCost())));
-            cell.setPaddingLeft(5);
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_LEFT);
-            table.addCell(cell);
-
-            cell = new PdfPCell(new Phrase(String.valueOf(bill.getColdWaterCost())));
-            cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-            cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            cell.setPaddingRight(5);
-            table.addCell(cell);
-
             PdfWriter.getInstance(document, out);
             document.open();
             document.add(new Paragraph("RACHUNEK # " + String.valueOf(bill.getId())));
-            document.add(table);
+            document.add(new Paragraph("Dla okresu: " + String.valueOf(bill.getMeasurement().getMonth()) + "." + String.valueOf(bill.getMeasurement().getYear())));
+            List orderedList = new List(List.ORDERED);
+            orderedList.add(new ListItem("Prad: " + String.valueOf(bill.getElectricityCost()) + " pln"));
+            orderedList.add(new ListItem("Zimna woda: " + String.valueOf(bill.getColdWaterCost()) + " pln"));
+            orderedList.add(new ListItem("Ciepla woda: " + String.valueOf(bill.getHotWaterCost()) + " pln"));
+            orderedList.add(new ListItem("Ogrzewanie: " + String.valueOf(bill.getHeatingCost()) + "  pln"));
+            document.add(orderedList);
         } catch (DocumentException ex) {
             throw new ServerException("error in generating pdf file");
         } catch (Exception e) {
