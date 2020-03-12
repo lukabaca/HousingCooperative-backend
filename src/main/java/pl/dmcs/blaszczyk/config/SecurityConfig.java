cbdbe60039f5c7f09322157c2ed4fa9b7b5feddc
lukasz <lkszblaszczyk@gmail.com>
2020-03-12 21:17:22 +0100

    small refactor

--- src/main/java/pl/dmcs/blaszczyk/config/SecurityConfig.java
+++ src/main/java/pl/dmcs/blaszczyk/config/SecurityConfig.java
@@ -1,27 +1,24 @@
 package pl.dmcs.blaszczyk.config;
+
 import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import pl.dmcs.blaszczyk.security.JWTAuthenticationEntryPoint;
 import pl.dmcs.blaszczyk.security.JWTAuthenticationFilter;
-import pl.dmcs.blaszczyk.security.JWTAuthorizationFilter;
-import pl.dmcs.blaszczyk.security.JWTConfig;
-import pl.dmcs.blaszczyk.service.AuthService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import pl.dmcs.blaszczyk.service.CustomUserDetailsService;
 
 @Configuration
@@ -49,13 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/webjars/**",
             "/auth/login",
             "/auth/register",
-            "/auth/activateAccount/**"
+            "/auth/activateAccount/**",
+            "/docs"
     };
-
-    private static final String[] no_Auth = {
-        "/**"
-    };
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -104,11 +97,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(ImmutableList.of("*"));
         configuration.setAllowedMethods(ImmutableList.of("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
